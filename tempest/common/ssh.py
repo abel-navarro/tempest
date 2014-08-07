d0d3d1be5a62fa4f2316a91463931f9d6d8f28d1
Albert Vico <albert.vico@midokura.com>
2014-08-07 12:08:35 +0200

    test inter vm ssh fix

--- tempest/common/ssh.py
+++ tempest/common/ssh.py
@@ -110,7 +110,6 @@ class Client(object):
                                 key_filename=self.key_filename,
                                 timeout=self.channel_timeout,
                                 pkey=self.pkey, sock=channel)
-                    ssh.agent
 
                     LOG.info("Tunnel connection %s@%s successfully created through localhost port:4000",
                              self.username, self.host)
@@ -160,6 +159,7 @@ class Client(object):
         ssh = self._get_ssh_connection()
         transport = ssh.get_transport()
         channel = transport.open_session()
+        agent_handler = paramiko.agent.AgentRequestHandler(channel)
         channel.fileno()  # Register event pipe
         channel.exec_command(cmd)
         channel.shutdown_write()
