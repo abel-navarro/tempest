38b9daf4df705b39aefdb2dacf5286fd290b2c55
Abel Navarro <abel@midokura.com>
2014-08-19 17:59:01 +0200

    log.info

--- tempest/scenario/midokura/test_network_basic_inter_vmconnectivity.py
+++ tempest/scenario/midokura/test_network_basic_inter_vmconnectivity.py
@@ -127,7 +127,7 @@ class TestNetworkBasicIntraVMConnectivity(scenario.TestScenario):
                                                    gateway_ip=gateway_ip,
                                                    gateway_username=gateway_username)
             result = remote_client.exec_command('ls -la')
-            pprint("result %s" % result)
+            LOG.info("result %s" % result)
         except Exception:
             LOG.exception('ssh to server failed')
             self._log_console_output()
